5c77b1281c03e9225c5c8ad03be6b1abea7a512f
김은선 <eunsunbsn@gmail.com>
2022-10-16 13:57:54 +0900

    모바일 웹 스케일 제어 수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
       { charset: "utf-8" },
       {
         name: "viewport",
-        content: "width=device-width, initial-scale=1, user-scale=no",
+        content:
+          "width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no",
       },
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
